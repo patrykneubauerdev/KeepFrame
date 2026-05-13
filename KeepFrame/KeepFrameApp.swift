dedf501356dc9e0c24ad379ba4f7abf33f02530d
PatrykJanNeubauer <patrykneubauerdev@gmail.com>
2026-05-13 17:56:30 +0200

    Initial Commit

---
+++ KeepFrame/KeepFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepFrameApp.swift
+//  KeepFrame
+//
+//  Created by Patryk Neubauer on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct KeepFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
